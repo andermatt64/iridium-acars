c5b8e9d65567a0d94787b3cb9498808ed60092b1
yying <andy@peruse.io>
2022-12-11 03:20:37 -0500

    Fix typo

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -z "$@" ]; then
-  exec /usr/bin/supervisord -c /etc/supervisord.conf --nodaemon
+  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
 else
   exec $@
 fi
