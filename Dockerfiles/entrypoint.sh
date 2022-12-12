5db813ec8c6d39a22106fce9a236b9f84ae89949
yying <andy@peruse.io>
2022-12-11 20:28:47 -0500

    Remove supervisor

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 
 if [ -z "$@" ]; then
-  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
+  python3 /root/iridium-toolkit/reassembler.py -m acars -a json zmq: > /var/log/acars.log &
+  python3 /root/iridium-toolkit/reassembler.py -m idapp > /var/log/idapp.log &
+  exec iridium-extractor -D 4 -q 1000 /root/airspy.conf | python3 /root/iridium-toolkit/iridium-parser.py -o zmq
 else
   exec $@
 fi
