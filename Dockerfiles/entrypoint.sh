f371fdf0562373d678802f07bf357dba4107bd30
yying <andy@peruse.io>
2023-01-15 17:32:56 -0500

    update

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -z "$@" ]; then
-  python3 /root/iridium-toolkit/reassembler.py -m acars -a json zmq: > /var/log/acars.log &
-  python3 /root/iridium-toolkit/reassembler.py -m idapp > /var/log/idapp.log &
-  exec iridium-extractor -D 4 -q 1000 /root/airspy.conf | python3 /root/iridium-toolkit/iridium-parser.py -o zmq
+  python3 /root/iridium-toolkit/reassembler.py -m acars zmq: > /var/log/acars.log &
+  python3 /root/iridium-toolkit/reassembler.py -m idapp zmq: > /var/log/idapp.log &
+  iridium-extractor -D 4 /root/airspy.conf | python3 /root/iridium-toolkit/iridium-parser.py -o zmq
 else
   exec $@
 fi
